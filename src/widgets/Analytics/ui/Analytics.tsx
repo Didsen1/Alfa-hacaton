b0ec927b214f8c6b0dda534bc5ebabc71568fe54
Igor Topal <i@itopal.ru>
2024-01-31 22:42:42 +0300

    complete analytics component layout

--- src/widgets/Analytics/ui/Analytics.tsx
+++ src/widgets/Analytics/ui/Analytics.tsx
@@ -1,11 +1,9 @@
 import { type FC } from 'react';
-import style from './Analytics.module.scss'
+import style from './Analytics.module.scss';
 import Chart from './Chart/Chart';
 import { type column } from '../model/types/chart';
 
-interface AnalyticsProps {
-
-}
+interface AnalyticsProps {}
 
 const taskColumns: column[] = [
   { amount: 10, name: 'Создано' },
@@ -23,13 +21,34 @@ const planColumns: column[] = [
 ];
 
 const Analytics: FC<AnalyticsProps> = () => {
-  const a = ''
+  const a = '';
   return (
-    <div className={style.chartWrapper}>
-      <Chart columns={taskColumns} width={404} heading='Статусы задач' color='#61BE76' />
-      <Chart columns={planColumns} width={320} heading='Статусы ИПР' color='#FF9352' />
+    <div className={style.Analytics}>
+      <h2 className={style.heading}>Текущие условия</h2>
+      <div className={style.indicatorsWrapper}>
+        <div className={style.indicator}>
+          <span className={style.indicatorValue}>10</span>
+          <span className={style.indicatorName}>ИПР</span>
+        </div>
+        <div className={style.indicator}>
+          <span className={style.indicatorValue}>40%</span>
+          <span className={style.indicatorName}>выполнено</span>
+        </div>
+        <div className={style.indicator}>
+          <span className={style.indicatorValue}>46</span>
+          <span className={style.indicatorName}>задач</span>
+        </div>
+        <div className={style.indicator}>
+          <span className={style.indicatorValue}>25%</span>
+          <span className={style.indicatorName}>выполнено</span>
+        </div>
+      </div>
+      <div className={style.chartWrapper}>
+        <Chart columns={taskColumns} width={404} heading="Статусы задач" color="#61BE76" />
+        <Chart columns={planColumns} width={320} heading="Статусы ИПР" color="#FF9352" />
+      </div>
     </div>
   );
 };
 
-export default Analytics
+export default Analytics;
