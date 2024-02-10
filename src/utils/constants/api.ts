0be78835c1a2294a5cce4a5cd83c364836de801b
Didsen1 <tihanovart@gmail.com>
2024-02-10 15:55:56 +0300

    Fix: url

--- src/utils/constants/api.ts
+++ src/utils/constants/api.ts
@@ -1,1 +1,1 @@
-export const BASE_URL = import.meta.env.BASE_URL === '/' ? 'http://51.250.6.208/' : import.meta.env.BASE_URL;
+export const BASE_URL = import.meta.env.BASE_URL === '/' ? 'https://alfa-plans.ddns.net/' : import.meta.env.BASE_URL;
